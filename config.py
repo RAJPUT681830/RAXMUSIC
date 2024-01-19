17323bef3b47cc20ab6c372e1765985470c31853
RAJPUT681830 <149851567+RAJPUT681830@users.noreply.github.com>
2024-01-19 20:34:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,9 +39,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/akshayxt/RAXMUSIC",
+    "https://github.com/RAJPUT681830/RAXMUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Rax")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Rax01")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
